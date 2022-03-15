8c5ce2b154dc3960638e3a756965197bc23a1518
blog00001 <69361681+blog00001@users.noreply.github.com>
2022-03-15 19:34:29 +0530

    ass done

---
+++ Assignment2/Program11.java
@@ -0,0 +1,10 @@
+//11. Write a Java program to print the area and perimeter of a circle. 
+class Program11
+{
+	public static void main(String argv[])
+	{
+		double Radius = 7.5;
+		final float Pi = 3.14f;
+		System.out.println("Area of circle is "+Pi*Radius*Radius);
+		System.out.println("Perimeter of circle is "+2*(Pi*Radius));
+}}
---
+++ Assignment2/Program9.java
@@ -0,0 +1,8 @@
+//9. Write a Java program to compute the specified expressions and print the output. 
+class Program9
+{
+public static void main(String argv[])
+{
+	double a = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+	System.out.println(a);
+}}
